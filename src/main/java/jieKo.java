b1893967f4659119d3e8185dfeeb974e3ca37216
ken <ken@qq.com>
2018-09-12 19:11:30 +0800

    添加了一个调用接口的功能

---
+++ src/main/java/jieKo.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.HttpURLConnection;
+
+import java.net.URL;
+
+
+    public class jieKo {
+        public static void main(String[] args) {
+            try {
+                RestUtil restUtil = new RestUtil();
+                String load = restUtil.load("", "");
+                System.out.println(load);
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+        }
+        public static class RestUtil{
+            public String load(String url,String query) throws Exception{
+                URL urlRest = new URL(url);
+                HttpURLConnection conn = (HttpURLConnection)urlRest.openConnection();
+                conn.setRequestMethod("Post");
+                conn.setDoOutput(true);
+                conn.setAllowUserInteraction(false);
+                PrintStream printStream = new PrintStream(conn.getOutputStream());
+                printStream.print(query);
+                printStream.close();
+
+                BufferedReader bReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                String line,resultStr="";
+
+                while(null != (line=bReader.readLine()))
+                {
+                    resultStr +=line;
+                }
+                System.out.println("3412412---"+resultStr);
+                bReader.close();
+
+                return resultStr;
+
+            }
+
+
+        }
+
+
+    }
+
+
