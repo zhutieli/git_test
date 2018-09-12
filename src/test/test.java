55c8e532bd7f5590184aa99d023aed866241a5c3
ken <ken@qq.com>
2018-09-12 17:42:29 +0800

    12313123

---
+++ src/test/test.java
@@ -0,0 +1,38 @@
+public static void main(String[] args) {
+        try {
+        RestUtil restUtil = new RestUtil();
+        String load = restUtil.load("", "");
+        System.out.println(load);
+        } catch (Exception e) {
+
+        e.printStackTrace();
+        }
+        }
+public static class RestUtil{
+    public String load(String url,String query) throws Exception{
+        URL urlRest = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection)urlRest.openConnection();
+        conn.setRequestMethod("Post");
+        conn.setDoOutput(true);
+        conn.setAllowUserInteraction(false);
+        PrintStream printStream = new PrintStream(conn.getOutputStream());
+        printStream.print(query);
+        printStream.close();
+
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        String line,resultStr="";
+
+        while(null != (line=bReader.readLine()))
+        {
+            resultStr +=line;
+        }
+        System.out.println("3412412---"+resultStr);
+        bReader.close();
+
+        return resultStr;
+
+    }
+
+
+}
